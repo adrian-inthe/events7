871d4bf276f3b204bbd6d8af2690b08747a7fced
Adrian <>
2024-10-31 23:02:52 +0100

    Handle CORS for local requests

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,18 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Allow localhost on any port for development purposes.
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin || origin.startsWith('http://localhost:')) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  });
+
   await app.listen(3000);
 }
 
