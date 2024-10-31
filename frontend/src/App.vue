3f578a7da53806452a15863de3c0a4a2e840e3dc
Adrian <>
2024-10-31 22:09:38 +0100

    Outfit7 it up!

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,4 +8,8 @@
 </script>
 
 <style>
+body {
+  /* Outfit7's main font-family */
+  font-family: O7TF Display, system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Open Sans, Helvetica Neue, sans-serif;
+}
 </style>
